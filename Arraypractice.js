9659d793e24bac52a2f91f114bc96d61e7455ac9
shivrajjajra <shivrajjajra00@gmail.com>
2023-07-25 23:23:41 +0530

    Array Notes

---
+++ Arraypractice.js
@@ -0,0 +1,152 @@
+// ***************arr.push()**************
+
+// let arr = [1,2,3,4,5]
+// arr.push(6);
+// console.log(arr);
+
+// ***************arr.pop()**************
+
+// let arr = [1,2,3,4,5]
+//  arr.pop();
+// console.log(arr);
+
+// ***************arr.unshift()**************
+
+// let arr = [1,2,3,4,5]
+// arr.unshift(8);
+// console.log(arr);
+
+// ***************arr.shift()**************
+
+// let arr = [1,2,3,4,5]
+//  arr.shift();
+// console.log(arr);
+
+// ***************arr.toString()**************
+
+// let arr = [1,2,"wecode",3,4,5]
+// console.log(arr.toString());
+
+// ***************arr.join()**************
+
+// let arr = [1,2,3,4,5]
+// console.log( arr.join("**"));
+
+//  let arr = [1,2,3,4,5]
+//  let ans = arr.join("$$")
+// console.log( ans)
+
+// ***************arr.reverse()**************
+
+//  let arr = [1,2,3,4,5]
+// arr.reverse()
+// console.log(arr)
+
+// ***************arr.at()**************
+
+// let arr = [1,2,3,4,5]
+// console.log(arr.at(-1))
+
+
+// ***************arr.concat()**************
+
+// let arr = [1,2,3,]
+// let ans = [4,5,6,7,8,9]
+// console.log(arr.concat(ans))
+
+// ***************arr.foreach()**************
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.forEach((value) => {
+//     console.log(value);
+// })
+
+// ***************arr.find()**************
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.find((value) => {
+//   return value > 3;
+// })
+// console.log(ans);
+
+// ***************arr.some()**************
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.some((value) => {
+//   return value > 3;
+// })
+// console.log(ans);
+
+// ***************arr.every()**************
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.every((value) => {
+//   return value > 3;
+// })
+// console.log(ans);
+
+// ***************arr.findindex()**************
+
+// let arr = [2,22,23,14,5]
+// let ans = arr.findIndex((value) => {
+//   return value > 4;
+// })
+// console.log(ans);
+
+// ***************arr.map()**************
+
+// let arr = [2,22,23,14,5]
+// let ans = arr.map((value) => {
+//   return value*2;
+// })
+// console.log(ans);
+
+// ***************arr.filter()**************
+
+// let arr = [2,12,23,14,5]
+// let ans = arr.filter((value) => {
+//   return value > 5 ;
+// })
+// console.log(ans);
+
+
+// ***************arr.reduce()**************
+
+// let arr = [2,3,4,6,10]
+// let ans = arr.reduce((prev,curr) => {
+//     console.log(arr);
+//   return prev,curr ;
+// })
+// console.log(ans);
+
+// let arr = [2,3,4,6,10]
+// let ans = arr.reduce((prev,curr) => {
+//     console.log(arr);
+//   return prev+curr ;
+// })
+// console.log(ans);
+
+// let arr = [2,3,4,6,10]
+// let ans = arr.reduce((prev,curr) => {
+//     console.log(arr);
+//   return prev+curr ;
+// },0)
+// console.log(ans);
+
+
+// let arr = ["we", "code", "we","Academy"];
+// let ans = arr.reduce((prev, curr) => {
+//     return prev + curr;
+// });
+
+// console.log(ans.length);
+
+// let arr = [1,2,3,4,5]
+
+// let ans = arr.reduce((prev,curr) => {
+//     console.log(prev,curr);
+//     return prev+curr
+// })
+// console.log(ans);
+
+
